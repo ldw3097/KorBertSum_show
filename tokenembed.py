989890252a87f31c6dbaf502ac24f9ab7340fc8a
Junseok Lee <shoveling42@gmail.com>
2022-08-14 14:42:02 +0900

    tokenembed.py completed

--- tokenembed.py
+++ tokenembed.py
@@ -1,8 +1,6 @@
 import os
 import json
 
-from pyparsing import line_end
-
 def morph_to_idx(morph):
     bert_dir = './1_bert_download_001_bert_morp_pytorch/001_bert_morp_pytorch'
     file_path = ''
@@ -12,6 +10,7 @@ def morph_to_idx(morph):
             file_path = bert_dir + '/' + entry
 
     with open(file_path) as f:
+        # OoV 문제는 UNK로 처리하는 대신 0으로 인덱스 넣어줌
         temp_list = [0 for i in morph]
         lines = f.readlines()
         for idx, item in enumerate(morph):
@@ -28,19 +27,22 @@ def morph_to_idx(morph):
             for line in lines:
                 if line.find(item) != -1:
                     temp_list[idx] = int(line.split('\t')[1].strip('\n'))
-                    break
-        print(temp_list)
+                    
+        return temp_list
 
 def main():
     train_f = './json_data/korean.train.1.json'
 
     # match word/morph with indices
-    # 0인 토큰 왜 뜨는지 잘 모르겠음
+    # UNK 토큰은 인덱스 0으로 처리
+    list_dict = list()
     with open(train_f, 'r') as file:
         data = json.load(file, strict=False)
         for i in range(len(data)):
             for j in range(len(data[i]['src'])):
-                morph_to_idx(data[i]['src'][j])
+                mydict = {}
+                mydict['src'] = morph_to_idx(data[i]['src'][j])
+                list_dict.append(mydict)
 
 if __name__ == '__main__':
     main()
