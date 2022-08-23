3b9aca563c16402c467cb2ccaf8e02b5a06d24a6
Junseok Lee <shoveling42@gmail.com>
2022-08-23 13:09:20 +0900

    src, tgt_txt completed

--- srctgt.py
+++ srctgt.py
@@ -1,71 +1,44 @@
 import json
 
-def src_txt():
-    news_path = r'./raw_data/train_sample_copy.json'
-    with open(news_path, 'r') as file:
+def src_txt(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file, strict=False)
-    
-        numofarts = len(data["documents"])
+
         src_list = list()
-        # create src_list
-        for i in range(numofarts):
-            mydict = {}
-            article = data["documents"][i]["text"]
-
-            # empty list delete
-            for sentence in article:
-                if len(sentence) == 0:
-                    del article[article.index(sentence)]
-            art_length = len(article)
-
-            sents = list()
-            for j in range(2, art_length):
-                # for each sentence
-                for idx in range(len(article[j])):
-                    sent = article[j][idx]["sentence"]
-                    sents.append(sent)
-
-            mydict['src_txt'] = sents
-            src_list.append(mydict)
+        for sng_art in data:
+            sent_list = []
+            for _, morphs in enumerate(sng_art['src']):
+                morphs.pop(0)
+                morphs.pop(len(morphs)-1)
+                sent = ' '.join(morphs)
+                sent_list.append(sent)
+            src_list.append(sent_list)
 
     return src_list
 
-def tgt_txt(src_list):
-    news_path = r'./raw_data/train_sample_copy.json'
-    with open(news_path, 'r') as file:
+def tgt_txt(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file, strict=False)
 
-        numofarts = len(data["documents"])
-
-        extra_indices_set = list()
-        # extract the indices of extractive sentences
-        for i in range(numofarts):
-            extractive = data["documents"][i]["extractive"]
-            extra_indices_set.append(extractive)
-
-        # create tgt_list
         tgt_list = list()
-        for i in range(numofarts):
-            mydict = {}
+        for sng_art in data:
+            sent_list = []
+            for _, morphs in enumerate(sng_art['tgt']):
+                morphs.pop(0)
+                morphs.pop(len(morphs)-1)
+                sent = ' '.join(morphs)
+                sent_list.append(sent)
             
-            sents = list()
-            for idx in extra_indices_set[0]:
-                sent = src_list[i]['src_txt'][idx-2]
-                sents.append(sent)
-            # update extra_indices_set
-            del extra_indices_set[0]
-
-            if len(extra_indices_set) == 0:
-                break
+            tgt_sent = '<p>'.join(sent_list)
+            tgt_list.append(tgt_sent)
             
-            mydict['tgt_txt'] = sents
-            tgt_list.append(mydict)
-
         return tgt_list
 
 def main():
-    src_list = src_txt()
-    tgt_list = tgt_txt(src_list)
+    # check file_path
+    file_path = './json_data/korean.train.1.json'
+    src_list = src_txt(file_path)
+    tgt_list = tgt_txt(file_path)
 
 if __name__ == "__main__":
     main()
