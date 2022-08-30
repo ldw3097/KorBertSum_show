daee6ac28d60a2800dea655c226b8e18328de8b3
Junseok Lee <shoveling42@gmail.com>
2022-08-30 23:08:19 +0900

    changes Kiwi to ETRI API, due to performance issue

--- article2json.py
+++ article2json.py
@@ -1,35 +1,37 @@
 # -*- coding: utf-8 -*-
-import argparse
-from ctypes.wintypes import tagRECT
-import time
-import os
-from tokenize import Token
-from turtle import ScrolledCanvas
-import pandas as pd
-import ast
+# import argparse
+# from ctypes.wintypes import tagRECT
+# import time
+# import os
+# from tokenize import Token
+# from turtle import ScrolledCanvas
+# import pandas as pd
+# import ast
+# import six
+# import glob
+# import numpy as np
+# from tqdm import tqdm
+# import urllib
+# from kiwipiepy import Kiwi
+from etri_api_scraper import do_lang
 import json
-import six
-import glob
-import numpy as np
-from tqdm import tqdm
-from kiwipiepy import Kiwi
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-mode', default='', type=str, choices=['train','valid','test'])
-    parser.add_argument('-news_dir', default='', type=str, help='target file to json')
-    parser.add_argument('-output', default='', type=str, help='json output directory')
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-mode', default='', type=str, choices=['train','valid','test'])
+    # parser.add_argument('-news_dir', default='', type=str, help='target file to json')
+    # parser.add_argument('-output', default='', type=str, help='json output directory')
 
     # parser.random_state("-random_state", default="", type=int, help="random state")
 
-    args = parser.parse_args()
-    mode = args.mode
-    news_dir = args.news_dir
-    output = args.output
+    # args = parser.parse_args()
+    # mode = args.mode
+    # news_dir = args.news_dir
+    # output = args.output
 
     # main process
-    news_path = r'./raw_data/train_sample_copy.json'
-    with open(news_path, 'r') as file:
+    file_path = r'./raw_data/train_sample_copy.json'
+    with open(file_path, 'r') as file:
         data = json.load(file, strict=False)
     
         src_list = list()
@@ -44,21 +46,17 @@ def main():
                     del article[article.index(sentence)]
             art_length = len(article)
 
-            temp_list2 = list()
+            sent_morphs = list()
             # analyze each sentence's morphs
             for j in range(2, art_length):
                 # for each sentence
                 for idx in range(len(article[j])):
-                    kiwi = Kiwi()
-                    temp_list1 = []
-                    tokenize_list = kiwi.tokenize(article[j][idx]["sentence"])[:-1]
+                    # do_lang(open-api key, sentence)
+                    tokenize_str = do_lang('584938b1-8a6a-4ff2-9a03-849c28d4eecc', article[j][idx]["sentence"])
                     # create src's 'word/morph' structures
-                    for z in range(len(tokenize_list)):
-                        token = tokenize_list[z].form + '/' + tokenize_list[z].tag
-                        temp_list1.append(token)
-                    temp_list2.append(temp_list1)
-            src_list.append(temp_list2)
-            print(("{} completed".format(i)))         
+                    token = tokenize_str.split()
+                    sent_morphs.append(token)
+            src_list.append(sent_morphs)
         print("src_list completed")
 
         extra_indices_set = list()
@@ -97,6 +95,7 @@ def main():
 
         with open(file_path, 'w', encoding='utf-8') as f:
             json.dump(list_dic, f, ensure_ascii=False)
+
         # temp = list()
         # for i,a in enumerate(tqdm(list_dic)):
         #     if (i+1)%6 != 0:
