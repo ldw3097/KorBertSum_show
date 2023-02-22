5b6945ee7491f1b3e80c739b1925289adc3b914a
shoveling42 <behindstarter42@gmail.com>
2023-02-22 22:18:58 +0900

    modification due to merge

--- list2tensor.py
+++ list2tensor.py
@@ -1,9 +1,6 @@
-from tokenembed import *
-from label import *
-from segmentembed import *
-from positionembed import *
-from srctgt import *
+import json
 import torch
+import embedding
 
 def news_to_input(token_id, label, segs, clss, src_txt, tgt_txt):
     b_data_dict = {"src": token_id,
@@ -18,28 +15,16 @@ def news_to_input(token_id, label, segs, clss, src_txt, tgt_txt):
     return b_list
 
 def main():
-    train_f = './json_data/korean.train.1.json'
+    train_file = './json_data/korean.train.1.json'
 
-    # match word/morph with indices
-    # UNK 토큰은 인덱스 0으로 처리
-    token_dict = list()
-    with open(train_f, 'r') as file:
-        data = json.load(file, strict=False)
-        for i in range(len(data)):
-            temp_dic = {}
-            for j in range(len(data[i]['src'])):
-                temp_src = morph_to_idx(data[i]['src'][j])
-                temp_dic['src'] = temp_src
-            token_dict.append(temp_dic)
+    tmp0 = embedding.tokenembed(train_file)
+    tmp1 = embedding.label(train_file)
+    tmp2 = embedding.seg_embed(train_file)
+    tmp3 = embedding.pos_embed(train_file)
+    tmp4 = embedding.src_txt(train_file)
+    tmp5 = embedding.tgt_txt(train_file)
 
-    tmp0 = token_dict
-    tmp1 = label()
-    tmp2 = segembed()
-    tmp3 = posembed()
-    tmp4 = src_txt()
-    tmp5 = 'hehe'
-
-    for idx in range(len(token_dict)):
+    for idx in range(len(tmp0)):
         inputdata = news_to_input(tmp0[idx]['src'], tmp1[idx]['label'], tmp2[idx]['segs'], tmp3[idx]['clss'], tmp4[idx]['src_txt'], tmp5)
         a = torch.tensor(inputdata)
         print(a)
