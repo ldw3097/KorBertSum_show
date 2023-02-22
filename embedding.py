cc60c0caaed05d23370b4a9de7ab435e32726582
shoveling42 <behindstarter42@gmail.com>
2023-02-22 22:17:30 +0900

    merge embedding functions

--- embedding.py
+++ embedding.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def label(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -84,14 +85,56 @@ def tgt_txt(file_path):
             tgt_list.append(tgt_sent)
             
         return tgt_list
+    
+def morph_to_idx(morph):
+    bert_dir = './1_bert_download_001_bert_morp_pytorch/001_bert_morp_pytorch'
+    file_path = ''
+    # check if vocab list is
+    for entry in os.listdir(bert_dir):
+        if entry.endswith('list'):
+            file_path = bert_dir + '/' + entry
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        # OoV 문제는 UNK로 처리하는 대신 0으로 변환
+        temp_list = [0 for i in morph]
+        lines = f.readlines()
+        for idx, item in enumerate(morph):
+            # [CLS]
+            if idx == 0:
+                temp_list[0] = 1
+                continue
+            # [SEP]
+            elif idx == len(morph)-1:
+                temp_list[len(morph)-1] = 2
+                continue
+            # others
+            for line in lines:
+                if line.find(item) != -1:
+                    temp_list[idx] = int(line.split('\t')[1].strip('\n'))
+                    
+        return temp_list
+
+def tokenembed(file_path):
+    # UNK 토큰은 인덱스 0으로 처리
+    token_dict = list()
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file, strict=False)
+        for i in range(len(data)):
+            mydict = {}
+            for j in range(len(data[i]['src'])):
+                temp_src = morph_to_idx(data[i]['src'][j])
+                mydict['src'] = temp_src
+            token_dict.append(mydict)
+    return token_dict
 
 def main():
-    file_path = './json_data/korean.train.1.json'
-    label_data = label(file_path)
-    segment_label = seg_embed(file_path)
-    pos_list = pos_embed(file_path)
-    src_list = src_txt(file_path)
-    tgt_list = tgt_txt(file_path)
+    train_file = './json_data/korean.train.1.json'
+    tmp0 = tokenembed(train_file)
+    tmp1 = label(train_file)
+    tmp2 = seg_embed(train_file)
+    tmp3 = pos_embed(train_file)
+    tmp4 = src_txt(train_file)
+    tmp5 = tgt_txt(train_file)
 
 if __name__ == "__main__":
     main()
